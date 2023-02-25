5dab599d7bcacd443b29d971191a90961cb6f9f1
Merci <habmercima@gmail.com>
2023-02-25 17:05:29 +0900

    registration page

--- app/src/main/java/com/example/taskie/MainActivity.kt
+++ app/src/main/java/com/example/taskie/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.taskie
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 //import android.os.Bundle
 //
@@ -12,7 +13,6 @@ import androidx.appcompat.app.AppCompatActivity
 
 
 import android.os.Bundle
-import android.view.View
 import android.widget.*
 
 /**
@@ -24,20 +24,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // get reference to all views
-        var etusername = findViewById<EditText>(R.id.et_user_name)
-        var etpassword = findViewById<EditText>(R.id.et_password)
-        var btnlogin = findViewById<Button>(R.id.btn_login)
-
-        // set on-click listener
-        btnlogin.setOnClickListener {
-            val username = etusername.text;
-            val password = etpassword.text;
-            Toast.makeText(this@MainActivity, username, Toast.LENGTH_LONG).show()
-
-            // your code to validate the user_name and password combination
-            // and verify the same
-
+        var createNewAccount = findViewById<TextView>(R.id.already_have_account)
+        createNewAccount.setOnClickListener{
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
         }
     }
 }
